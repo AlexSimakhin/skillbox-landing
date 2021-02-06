18f119d6752495b01de62718d5f08e723614f665
AlexSimakhin <alexsimahin@gmail.com>
2021-02-06 17:45:36 +0200

    feat: (time end / WIP)

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,15 @@
 // Это пример файла со скриптами. Можете писать здесь код, который будет на странице.
+const form = document.querySelector('.contacts__tel-button');
+const container = document.querySelector('.popup-container');
+const btn = document.querySelector('.light-button.send-order');
+
+form.onclick = event => {
+  console.log(container);
+  event.preventDefault();
+  container.style.display = 'flex';
+};
+
+btn.onclick = event => {
+  console.log(btn);
+  container.style.display = 'none';
+};
